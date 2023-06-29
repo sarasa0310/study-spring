beb4f3b8c7f63d35a7925b113f382c2278af4f04
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-06-29 13:01:59 +0900

    Chore: RestTemplate 활용해서 api 호출 학습 완료

--- src/main/java/com/jimmy/studyspring/StudySpringApplication.java
+++ src/main/java/com/jimmy/studyspring/StudySpringApplication.java
@@ -1,13 +1,35 @@
 package com.jimmy.studyspring;
 
+import com.jimmy.studyspring.quoters.QuoteResource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @SpringBootApplication
 public class StudySpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudySpringApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner runner(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            QuoteResource quote = restTemplate.getForObject(
+                    "http://localhost:8080/api/random", QuoteResource.class
+            );
+            log.info(quote.toString());
+        };
     }
 
 }
