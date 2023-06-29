4fa2c67f83935cd32b318b14998851ac99b110f0
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-06-29 14:46:56 +0900

    Feat: spring-data-rest와 hal-explorer 기능 테스트
    - Entity와 Repository만으로 간단한 REST API를 만들고 브라우저에서 볼 수 있는 spring-data-rest와 hal-explorer 테스트

--- src/main/java/com/jimmy/studyspring/quoters/QuoteRepository.java
+++ src/main/java/com/jimmy/studyspring/quoters/QuoteRepository.java
@@ -1,6 +1,8 @@
 package com.jimmy.studyspring.quoters;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 }
