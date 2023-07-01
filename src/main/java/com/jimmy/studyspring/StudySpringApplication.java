ee15e89cfacec7d2f5a579d9b12380b0c44c8364
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-07-01 14:30:50 +0900

    Feat: 파일 업로드 연습을 위한 초기 storage 패키지 파일 추가

--- src/main/java/com/jimmy/studyspring/StudySpringApplication.java
+++ src/main/java/com/jimmy/studyspring/StudySpringApplication.java
@@ -2,8 +2,10 @@ package com.jimmy.studyspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class StudySpringApplication {
 
     public static void main(String[] args) {
