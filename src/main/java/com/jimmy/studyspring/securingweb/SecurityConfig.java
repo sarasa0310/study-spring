270bec64a928218168f6f3e5bcd263503ab73d8c
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-07-07 12:07:56 +0900

    Feat: MvcConfig와 SecurityConfig 추가
    - MvcConfig에서 URL 경로에 맞춰 적절한 View 연결
    - SecurityConfig에서 인증 설정 및 테스트용 인메모리 유저 2명 등록
    
    <특이사항> 최신 안정 버전 6.1.1
    - Spring Security 6 버전에 들어오면서 antMatcher, mvcMatcher가 Deprecated되어 삭제되고 requestMatchers로 통일됨

---
+++ src/main/java/com/jimmy/studyspring/securingweb/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.jimmy.studyspring.securingweb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(login -> login
+                        .loginPage("/login").permitAll())
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        UserDetails jimmy =
+                User.withDefaultPasswordEncoder()
+                        .username("jimmy")
+                        .password("abcd1234")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user, jimmy);
+    }
+
+}
